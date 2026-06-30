ae1a85d596476404ba7b52edb9b476555d7add31
Astra-max <waoremaxwel17@gmail.com>
2026-06-30 23:53:04 +0300

    migrations: role table will contain user role migration file

---
+++ e-commerce-backend/src/database/migrations/V1__create_role.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS roles (
+    role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
+    role_name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
