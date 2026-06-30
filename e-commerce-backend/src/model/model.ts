08e07c00b42ba670bd80d53d37eb055c8f525088
Astra-max <waoremaxwel17@gmail.com>
2026-07-01 02:45:25 +0300

    db: connecting to db via connection string

--- e-commerce-backend/src/model/model.ts
+++ e-commerce-backend/src/model/model.ts
@@ -3,17 +3,21 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool: Pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL as string,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool
   .connect()
-  .then(() => console.log("Connected to Database"))
-  .catch((err) => console.error("Failed to connect to DB", err));
+  .then((client) => {
+    console.log("Connected to Database");
+    client.release();
+  })
+  .catch((err) => {
+    console.error("Failed to connect to DB", err);
+  });
 
 export default pool;
