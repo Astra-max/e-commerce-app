27b489b9a5b9b658dcfd994cb395fcca85641859
Astra-max <waoremaxwel17@gmail.com>
2026-07-01 21:43:09 +0300

    migration: removed old migration files from src folder

--- e-commerce-backend/src/database/migrations/V1__create_role.sql
+++
@@ -1,8 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE TABLE IF NOT EXISTS roles (
-    role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
-    role_name VARCHAR(50) NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
