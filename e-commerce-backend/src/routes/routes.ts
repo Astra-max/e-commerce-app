7d94d9d3c603123064e8112a12674ab80ef88b3e
Astra-max <waoremaxwel17@gmail.com>
2026-07-03 01:58:05 +0300

    refactor: general project refactor

--- e-commerce-backend/src/routes/routes.ts
+++ e-commerce-backend/src/routes/routes.ts
@@ -11,7 +11,7 @@ import {
   HandleReduceQuantity,
 } from "../controllers/quantity";
 import { getAllUsersController } from "../controllers/users.controllers";
-import { authRateLimiter, generalRateLimiter } from "../middleware/rate-limiter";
+import { authRateLimiter, generalRateLimiter } from "../middleware/rate.limit.middleware";
 
 
 export const router = express.Router();
@@ -21,13 +21,13 @@ router.post("/auth/login", authRateLimiter, HandleLogin);
 router.post("/auth/register", authRateLimiter, HandleSignUP);
 
 // users routes
-router.get("/users", generalRateLimiter, getAllUsersController);
+router.get("/users",  getAllUsersController);
 
 //cart routes
-router.post("/cart", generalRateLimiter, HandleAddItem);
-router.delete("/cart/:userId/:productId", generalRateLimiter, HandleRemoveItem);
-router.put("/quantity/add", generalRateLimiter, HandleAddQuantity);
-router.put("/quantity/reduce", generalRateLimiter, HandleReduceQuantity);
-router.get("/cart/:userId", generalRateLimiter, HandleGetCart);
-router.get("/total/:userId", generalRateLimiter, HandleGetAmount);
-router.put("/total/:productId", generalRateLimiter, HandleAddTotal);
+router.post("/cart",HandleAddItem);
+router.delete("/cart/:userId/:productId",  HandleRemoveItem);
+router.put("/quantity/add", HandleAddQuantity);
+router.put("/quantity/reduce", HandleReduceQuantity);
+router.get("/cart/:userId", HandleGetCart);
+router.get("/total/:userId",  HandleGetAmount);
+router.put("/total/:productId", HandleAddTotal);
