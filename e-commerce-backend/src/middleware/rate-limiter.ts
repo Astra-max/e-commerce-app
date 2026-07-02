72dc5e905b710ea6a9ee44d7c8a6b49b003b1936
Astra-max <waoremaxwel17@gmail.com>
2026-07-03 01:10:29 +0300

    feat: create auth and general rate limiter for api routes

--- e-commerce-backend/src/middleware/rate-limiter.ts
+++ e-commerce-backend/src/middleware/rate-limiter.ts
@@ -1,28 +1,20 @@
-import { NextFunction, Request, Response } from "express";
+import { rateLimit } from "express-rate-limit";
 
-/**
- * Handles general rate limiter
- */
-export function GeneralRateLimiter(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  let count = 3;
-  count -= 1;
-  console.log(`Allowed count ${count}`)
-  if (count <= 0) {
-    console.log("Request blocked: too many requests");
-    return res.json({ message: "Too many request try later" });
-  }
-  return next();
-}
 
-export default function RequestLimit(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  console.log(req.method, req.url);
-  next();
-}
+// general rate limiter for all routes
+export const generalRateLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 6, // Limit each IP to 6 requests per windowMs
+  message: "Too many requests, please try again later.",
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+// authentication rate limiter for login and signup routes
+export const authRateLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 5, // Limit each IP to 5 requests per windowMs
+  message: "Too many requests, please try again later.",
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
