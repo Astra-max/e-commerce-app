9805ab5ebf6962c51b1cd5200538f42deec09a89
Astra-max <waoremaxwel17@gmail.com>
2026-07-06 23:05:55 +0300

    feat: implemented function that perform CRUD operations to db with cart data

--- e-commerce-backend/src/repository/cart.respository.ts
+++ e-commerce-backend/src/repository/cart.respository.ts
@@ -1,22 +1,81 @@
-import pool from "../model/model";
+import pool from "../config/dbConnect";
 import { logger } from "../util/logger";
-import { getAllItemsQuery } from "../query/cart.query"
+import {
+    deleteAllItemsQuery,
+    deleteSingleItemQuery,
+    getAllItemsQuery,
+    getSingleItemQuery,
+    updateItemQuery,
+} from "../query/cart.query";
+import { CartItem } from "../model/cart.model";
+import { addItemQuery } from "../query/cart.query";
 
-
-export const getAllItemsRepo = async () => {
+export const getAllItemsRepo = async (): Promise<CartItem[]> => {
     try {
-        const allItems = await pool.query(getAllItemsQuery)
+        const allItems = await pool.query(getAllItemsQuery);
+        return allItems.rows;
     } catch (error: any) {
-        logger.warn(`Failed to get all cart items ${error}`)
+        logger.warn(`Failed to get all cart items ${error}`);
     }
-}
+    return [] as CartItem[];
+};
 
-export const getSingleItemRepo = async () => {}
+export const getSingleItemRepo = async (
+    itemId: string,
+): Promise<CartItem | null> => {
+    try {
+        const singleItem = await pool.query(getSingleItemQuery, [itemId]);
+        return singleItem.rows[0] ?? null;
+    } catch (error: any) {
+        logger.warn(`Failed to get single cart item ${error}`);
+    }
+    return null;
+};
 
-export const updateItemRepo = async () => {}
+export const updateItemRepo = async (
+    itemId: string,
+): Promise<CartItem | null> => {
+    // Implementation for updating a cart item
+    try {
+        const updateItem = await pool.query(updateItemQuery, [itemId]);
+        return updateItem.rows[0] ?? null;
+    } catch (error: any) {
+        logger.warn(`Failed to update item ${error}`);
+    }
+    return null;
+};
 
-export const saveItemRepo = async () => {}
+export const saveItemRepo = async () => {
+    // Implementation for saving a cart item
+    try {
+        const saveItem = await pool.query(addItemQuery);
+        return;
+    } catch (error) {
+        logger.warn(`Failed to save cart item to db ${error}`);
+    }
+    return;
+};
 
-export const deleteSingleItemRepo = async () => {}
+export const deleteSingleItemRepo = async (
+    itemId: string,
+): Promise<boolean> => {
+    // Implementation for deleting a single cart item
+    try {
+        const deletedItem = await pool.query(deleteSingleItemQuery, [itemId]);
+        return true;
+    } catch (error) {
+        logger.warn(`Failed to delete cart item ${error}`);
+    }
+    return false;
+};
 
-export const deleteAllItemsRepo = async () => {}
+export const deleteAllItemsRepo = async (): Promise<boolean> => {
+    // Implementation for deleting all cart items
+    try {
+        const deleteAll = await pool.query(deleteAllItemsQuery);
+        return true;
+    } catch (error) {
+        logger.warn(`Failed to clear cart table ${error}`);
+    }
+    return false;
+};
