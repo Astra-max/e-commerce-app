b72de933961da339b12da735a18126cd7d72612d
Astra-max <waoremaxwel17@gmail.com>
2026-07-06 23:06:59 +0300

    feat: moved cart type to models coz it defines the structure of th database table

---
+++ e-commerce-backend/src/model/cart.model.ts
@@ -0,0 +1,11 @@
+export interface CartItem {
+    productId: string;
+    name: string;
+    description: string;
+    category: string;
+    quantity: string;
+    status: string;
+    amount: number;
+    userId: string;
+    image: string;
+}
