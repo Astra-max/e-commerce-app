2b70c3f5140e15483db5d6b5013762328a1ae526
Astra-max <waoremaxwel17@gmail.com>
2026-07-20 01:21:56 +0300

    feat: contains and or will contain ui that supports ui from view/

---
+++ e-commerce/src/components/pages/4u.tsx
@@ -0,0 +1,10 @@
+import "../../styles/app.css"
+
+// for you page
+const FORYOU = () => {
+  return (
+    <div className="about-cont">For you product page</div>
+  )
+}
+
+export default FORYOU                      
