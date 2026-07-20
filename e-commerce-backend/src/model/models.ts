eacc8985ab2dae983dc8a2d340f6318106167764
Astra-max <waoremaxwel17@gmail.com>
2026-07-21 01:21:51 +0300

    feat: product type defines the structure of the product object

---
+++ e-commerce-backend/src/model/models.ts
@@ -0,0 +1,20 @@
+export interface CartItem {
+    productId: string;
+    productName: string;
+    productDescription: string;
+    productCategory: string;
+    productPrice: string;
+    productImage: string;
+    quantity: number;
+    userId: string;
+}
+
+export interface ProductType {
+    productId: string;
+    productName: string;
+    productDescription: string;
+    productCategory: string;
+    productPrice: string;
+    productImage: string;
+    quantity: number;
+}
