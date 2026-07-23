146fa0a71a353d6cf3ad72f25082d3673dc5310c
Astra-max <waoremaxwel17@gmail.com>
2026-07-23 04:58:54 +0300

    refactor: moved env file read and variable validation from server.ts

--- e-commerce-backend/src/server.ts
+++ e-commerce-backend/src/server.ts
@@ -1,20 +1,19 @@
 import express from "express";
-import env from "dotenv";
 import { router } from "./routes/routes";
 import { generalRateLimiter } from "./middleware/rate.limit.middleware";
 import corsMiddleware from "./middleware/cors.middleware";
-
-env.config();
+import { LoadServerCfg } from "./config/loadEnv";
+import { logger } from "./util/logger";
 
 const app = express();
 app.use(corsMiddleware);
 app.use(express.json());
 app.use(generalRateLimiter);
 
-const PORT = process.env.SERVER_PORT || 5500;
+const cfg = LoadServerCfg()
 
 app.use("/api/v1", router);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(cfg.port, () => {
+  logger.info(`Server running on http://localhost:${cfg.port}`);
 });
