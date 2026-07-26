29acf5d2f921b9e872bc5fb7a8cfa41c721ca94a
Astra-max <waoremaxwel17@gmail.com>
2026-07-26 23:47:39 +0300

    refactor: renamed util to services whih contains server call

---
+++ e-commerce/src/services/axios.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getAccessToken } from "./token";
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  withCredentials: true, // Include cookies in requests
+});
+
+API.interceptors.request.use((config) => {
+  const token = getAccessToken();
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default API;
---
+++ e-commerce/src/services/refreshToken.ts
@@ -0,0 +1,33 @@
+import API from "./axios";
+import { setAccessToken } from "./token";
+
+
+
+API.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const original = error.config;
+
+    if (error.response?.status === 401 && !original._retry) {
+      original._retry = true;
+
+      try {
+        const res = await API.post("/auth/refresh");
+
+        const newToken = res.data.accessToken;
+
+        setAccessToken(newToken);
+
+        original.headers.Authorization = `Bearer ${newToken}`;
+
+        return API(original);
+      } catch (err) {
+        setAccessToken(null);
+        window.location.href = "/auth/login";
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
